4fb63a3a72fe0fd276dc6626fe8310ee1154ead6
Meet Patel <meet241patel@gmail.com>
2025-05-11 00:30:40 -0400

    movies component updated

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,7 @@ import Star from "./assets/glowing-star.png";
 import Party from "./assets/partying-face.png";
 import Navbar from "./components/Navbar/Navbar";
 import MovieList from "./components/MovieList/MovieList";
+import SingleMovie from "./components/MovieList/SingleMovie";
 
 const App = () => {
     return (
@@ -17,6 +18,7 @@ const App = () => {
                     <Route path="/" element={<MovieList type='popular' title='Popular' emoji={Fire} />}/>
                     <Route path="/top_rated" element={<MovieList type='top_rated' title='Top Rated' emoji={Star} />}/>
                     <Route path="/upcoming" element={<MovieList type='upcoming' title='Upcoming' emoji={Party} />}/>
+                    <Route path="/movie/:movieId" element={<SingleMovie />} />
                 </Routes>
             </main>
         </div>
--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "./MovieCard.css";
 import Star from "../../assets/star.png";
 
 const MovieCard = ({ movie }) => {
     return (
-        <a
-            href={`https://www.themoviedb.org/movie/${movie.id}`}
-            target='_blank'
+        <Link
+            to={`/movie/${movie.id}`}
             className='movie_card'>
             <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
@@ -34,7 +33,7 @@ const MovieCard = ({ movie }) => {
                     {movie.overview.slice(0, 100) + "..."}
                 </p>
             </div>
-        </a>
+        </Link>
     );
 };
 
